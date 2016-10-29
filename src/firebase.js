725bb63f4d9ddc48a98e0dee8ea93b4543622d35
Tudor Gergely <tudorgergely@gmail.com>
2016-10-29 20:47:53 +0300

    Add firebase, material- design and redux-observables

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA_QWaUgaoVD18QT6BlItQPHYgOlQ0XQz0",
+    authDomain: "tvshows-cacae.firebaseapp.com",
+    databaseURL: "https://tvshows-cacae.firebaseio.com",
+    storageBucket: "tvshows-cacae.appspot.com",
+    messagingSenderId: "835906598601",
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
